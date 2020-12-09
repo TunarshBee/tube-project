27ff9d228e841e7391e2ac175dff4db89d30d669
unknown <tunarshreal@gmail.com>
2020-12-09 14:51:37 -0200

    testing the click render

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import VideoList from './VideoList'
 import Youtube from '../apis/Youtube'
+import VideoDetail from "./VideoDetail";
 
 export class App extends Component {
 
-    state ={ videos :[], selectedVideos: null }
+    state ={ videos :[], selectedVideo: null }
 
   onTermSubmit = async term => {
    const resp = await Youtube.get('/search', {params:{
@@ -14,14 +15,15 @@ export class App extends Component {
     this.setState({videos: resp.data.items})
   };
 
-  onVideoSelect(videos){
-    console.log('From the App', videos )
+  onVideoSelect= videos=>{
+    this.setState({selectedVideo: videos})
   }
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo}/>
         <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
       </div>
     );
---
+++ src/components/VideoDetail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function VideoDetail({video}) {
+    
+    if(!video){
+        return <div>Loading....</div>
+    }
+
+    return (
+        <div>
+            {video.snippet.title}
+        </div>
+    )
+}
