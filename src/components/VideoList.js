4cd3c431638d97d5012618d8c531b00815e35a8a
unknown <tunarshreal@gmail.com>
2020-12-10 09:33:48 -0200

    Griding layout

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import VideoItem from './VideoItem'
-const VideoList=({videos, onVideoSelect})=> {
-
-    const renderedList = videos.map((video)=>{
-        return(
-            <VideoItem onVideoSelect={onVideoSelect} video={video} />
-        )
-    })
-
+import React from "react";
+import VideoItem from "./VideoItem";
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map((video) => {
     return (
-        <div className="ui relaxed divided list">
-            {renderedList}
-        </div>
-    )
-}
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
+  });
+
+  return <div className="ui relaxed divided list">{renderedList}</div>;
+};
 export default VideoList;
